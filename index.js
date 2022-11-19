b0b2d8fa8b93f70c751670f2cbc475f9fc2fd0d2
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-11-18 19:30:12 -0800

    renaming, other stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// API KEYS
+
+// ertrack api
+all_hospitals_api = "https://ertrack.net/api/hospitals/"
+one_hospital_api = "https://ertrack.net/api/hospital/"
+hospital_data = "/metadata/"
+hospital_wait = "/history/"
+
+// maps data api
+
+//...
+
