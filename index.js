f66a0b3af9305a14fb0ee55fee28bdc3ecfbf229
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-11-18 20:19:31 -0800

    started writing some helpful functions

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+import hospital from './hospitals.json' assert {type: 'json'}; // get hospital data w/o fetch
+hospital_data = JSON.parse(hospital);
+
+// general params
+
+MAX_RADIUS = 20; // in miles
+MAX_HOSPITALS = 5;
 
 // API KEYS
 
@@ -11,3 +18,22 @@ hospital_wait = "/history/"
 
 //...
 
+
+
+// api fetching functions
+
+function fetchAllHospitals() { // TODO
+    
+    return response;
+}
+
+function coordinatesToMiles(long1, lat1, long2, lat2) {
+    distance = sqrt((long2 - long1) ** 2 + (lat2 - lat1) ** 2);
+    distance *= 60
+    return distance;
+}
+
+function findHospitalsNear(longitude, latitude, count) {
+    hospital_data = findHospitals()
+
+}
