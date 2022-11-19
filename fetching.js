a187a0a900fda4f03d00f02f5a52cedbab282b4c
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-11-19 13:37:21 -0800

    Update fetching.js

--- fetching.js
+++ fetching.js
@@ -49,7 +49,7 @@ function updateDistance(longitude, latitude, hospital_data) {
     console.log(longitude);
     console.log(latitude);
     for (i in hospital_data) { 
-        getDistance(parseFloat(hospital_data[i]['lat']),latitude,hospital_data[i]['longitude'], longitude); 
+        hospital_data[i]['dist'] = getDistance(parseFloat(hospital_data[i]['lat']),latitude,hospital_data[i]['longitude'], latitude, longitude); 
     }
 }
 
@@ -89,10 +89,10 @@ async function get_hospital_wait(hospital_data) {
 }
 
 // console.log('process_hospital_name of "Doctors On Duty - Seaside" : ' + process_hospital_name("Doctors On Duty - Seaside"));
-// findHospitalsNear(37.396283, -122.115551, 5).then((response) => console.log(response));
+findHospitalsNear(37.396283, -122.115551, 5).then((response) => console.log(response));
 
 
-demo_hospital_data = {"address":"2500 Grant Road, Mountain View, CA 94040","county":"Santa Clara County","fips":"060855099011013","hospital_id":"551","hospital_name":"Emergency Room (Mountain View)","lat":"37.3691517","lng":"-122.0795279","state":"CA","type_id":"1"}
-console.log(Math.sqrt((parseFloat(demo_hospital_data["lat"]) - 0) ** 2 + (parseFloat(demo_hospital_data["lng"]) - 0) ** 2));
-console.log(getDistance(37.3691517, 0, -122.0795279 , 0));
+// demo_hospital_data = {"address":"2500 Grant Road, Mountain View, CA 94040","county":"Santa Clara County","fips":"060855099011013","hospital_id":"551","hospital_name":"Emergency Room (Mountain View)","lat":"37.3691517","lng":"-122.0795279","state":"CA","type_id":"1"}
+// console.log(Math.sqrt((parseFloat(demo_hospital_data["lat"]) - 0) ** 2 + (parseFloat(demo_hospital_data["lng"]) - 0) ** 2));
+// console.log(getDistance(37.3691517, -122.0795279, 0 , 0));
 // get_hospital_wait(demo_hospital_data).then((response) => console.log(response));
