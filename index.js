126c835ea1bdaee31e49dd2ebc9efee72405188e
LordOfDeadbush <37131666+LordOfDeadbush@users.noreply.github.com>
2022-11-19 18:57:29 -0800

    testing pages

--- index.js
+++ index.js
@@ -1,2 +1,141 @@
+// general params
 
+const MAX_RADIUS = 0.33; // in miles
+const HOSPITALS = 5;
 
+// API KEYS
+
+// ertrack api
+all_hospitals_api = "https://ertrack.net/api/hospitals/";
+one_hospital_api = "https://ertrack.net/api/hospital";
+hospital_data = "/metadata/";
+hospital_wait_url = "https://ertrack.net/";
+
+// hospital api bs
+
+function pivotImplementation(arr, start, end) {
+    const pivotValue = arr[end];
+    let pivotIndex = start;
+    for (let i = start; i < end; i++) {
+        if (arr[i]["dist"] > pivotValue) {
+            [arr[i]["dist"], arr[pivotIndex]["dist"] = [arr[pivotIndex]["dist"], arr[i]["dist"]]];
+            pivotIndex++;
+        }
+    }
+    [arr[pivotIndex]["dist"], arr[end]["dist"] = [arr[end]["dist"], arr[pivotIndex]["dist"]]];
+    return pivotIndex;
+}
+
+function qsRecursive(arr, start, end) {
+    if (start >= end) {
+        return;
+    }
+
+    let index = pivotImplementation(arr, start, end);
+
+    qsRecursive(arr, start, index - 1);
+    qsRecursive(arr, index + 1, end);
+}
+
+// getting hospitals
+
+async function fetchAllHospitals() {
+    const response = await fetch(all_hospitals_api);
+    hospital_data = await response.json();
+    for (i in hospital_data) {
+        if (hospital_data[i]["lng"] == 0 || hospital_data[i]["lng"] == "None") {
+            hospital_data[i]["lng"] = Number.MAX_SAFE_INTEGER;
+        }
+    }
+    // console.log(hospital_data);
+    return hospital_data;
+}
+
+// function findDistance(long1, lat1, long2, lat2) {
+//     distance = Math.abs(Math.sqrt(((long2 - long1) ** 2) + ((lat2 - lat1) ** 2)));
+//     // distance *= 60
+//     // console.log(distance);
+//     return distance;
+// }
+
+function getDistance(lat1, lng1, lat2, lng2) {
+    return Math.sqrt((lat1 - lat2) ** 2 + (lng1 - lng2) ** 2);
+}
+
+function updateDistance(longitude, latitude, hospital_data) {
+    // console.log(longitude);
+    // console.log(latitude);
+    for (i in hospital_data) {
+        hospital_data[i]["dist"] = getDistance(
+            parseFloat(hospital_data[i]["lat"]),
+            hospital_data[i]["lng"],
+            latitude,
+            longitude
+        );
+    }
+    return hospital_data;
+}
+
+async function findHospitalsNear(longitude, latitude, count) {
+    // TODO make sure to do everything that uses this data within an async function
+    // TODO get location here
+    hospital_data = await fetchAllHospitals();
+    // console.log(longitude, latitude);
+    hospital_data = updateDistance(longitude, latitude, hospital_data);
+    hospital_data = await update_wait_times(hospital_data);
+    // hospital_data.sort((a,b) => a["dist"] - b["dist"]);
+    // qsRecursive(hospital_data, 0, hospital_data.length - 1)
+    hospital_data.sort((a, b) => a["dist"] - b["dist"]);
+    // hospital_data.reverse();
+    // console.log(hospital_data);
+    hospitals = [];
+    for (i in hospital_data) {
+        if (hospitals.length >= count) break; //|| coordinatesToMiles(longitude, latitude, i["lng"], i["lat"])
+        if (hospital_data[i]["wait"] != "closed" && hospital_data[i]["dist"] <= MAX_RADIUS) hospitals.push(hospital_data[i]);
+    }
+    return hospitals;
+}
+
+function process_hospital_name(name) {
+    name = name.replaceAll("(", "");
+    name = name.replaceAll("- ", "");
+    name = name.replaceAll(" ", "-");
+    name = name.toLowerCase();
+    return name;
+}
+
+// async function get_hospital_wait(hospital_data) {
+//     url =  hospital_wait_url + hospital_data['hospital_id'] + '/' + process_hospital_name(hospital_data['hospital_name']);
+
+//     // await fetch(hospital_wait_url);
+//     console.log(url);
+//     const response = await fetch(url);
+//     hospital_data = await response.text();
+//     return hospital_data;
+// }
+
+async function update_wait_times(ext_hospital_data) {
+    const response = await fetch(hospital_wait_url);
+    hospital_data = await response.text();
+    i = hospital_data.indexOf("var data = ") + 11;
+    hospital_data_raw = "";
+    while (hospital_data[i - 1] != "}") {
+        hospital_data_raw += hospital_data[i];
+        i++;
+    }
+    hospital_data_json = JSON.parse(hospital_data_raw); // the 6th index is the wait time
+    for (i = 0; i < ext_hospital_data.length; i++) {
+        // console.log(ext_hospital_data[i]["hospital_id"]);
+        try {
+            ext_hospital_data[i]["wait"] =
+                hospital_data_json[ext_hospital_data[i]["hospital_id"]][6];
+        } catch (e) {
+            ext_hospital_data[i]["wait"] = "closed";
+        }
+    }
+
+    return ext_hospital_data;
+}
+
+window.navigator.geolocation.getCurrentPosition((position) => 
+    findHospitalsNear(position.coords.longitude, position.coords.latitude, HOSPITALS));
--- location_script.js
+++ location_script.js
@@ -1,119 +1,119 @@
-const mainSection = document.getElementById("main");
-const actions = document.getElementById("actions");
-const error = document.getElementById("error");
-const errorMessage = document.getElementById("errorMessage");
-const longitude = document.getElementById("longitude");
-const latitude = document.getElementById("latitude");
-const accuracy = document.getElementById("accuracy");
-const altitude = document.getElementById("altitude");
-const altitudeAccuracy = document.getElementById("altitudeAccuracy");
-const heading = document.getElementById("heading");
-const speed = document.getElementById("speed");
-const time = document.getElementById("time");
+// const mainSection = document.getElementById("main");
+// const actions = document.getElementById("actions");
+// const error = document.getElementById("error");
+// const errorMessage = document.getElementById("errorMessage");
+// const longitude = document.getElementById("longitude");
+// const latitude = document.getElementById("latitude");
+// const accuracy = document.getElementById("accuracy");
+// const altitude = document.getElementById("altitude");
+// const altitudeAccuracy = document.getElementById("altitudeAccuracy");
+// const heading = document.getElementById("heading");
+// const speed = document.getElementById("speed");
+// const time = document.getElementById("time");
 
-const currentLocationBtn = document.getElementById("currentLocationBtn");
-const liveLocationBtn = document.getElementById("liveLocationBtn");
-const cancelLiveLocationBtn = document.getElementById("cancelLiveLocationBtn");
+// const currentLocationBtn = document.getElementById("currentLocationBtn");
+// const liveLocationBtn = document.getElementById("liveLocationBtn");
+// const cancelLiveLocationBtn = document.getElementById("cancelLiveLocationBtn");
 
-function main() {
-  if(Boolean(window.navigator.geolocation)) {
-    mainSection.style.display = "flex";
-    currentLocationBtn.onclick = function (e) {
-      getLocation();
-    };
-    liveLocationBtn.onclick = function (e) {
-      liveLocation();
-    };
-  } else {
-      alert("error");
-      mainSection.style.display = "none";
-      actions.style.display = "none";
-      error.style.display = "block";
-  }
-}
+// function main() {
+//   if(Boolean(window.navigator.geolocation)) {
+//     mainSection.style.display = "flex";
+//     currentLocationBtn.onclick = function (e) {
+//       getLocation();
+//     };
+//     liveLocationBtn.onclick = function (e) {
+//       liveLocation();
+//     };
+//   } else {
+//       alert("error");
+//       mainSection.style.display = "none";
+//       actions.style.display = "none";
+//       error.style.display = "block";
+//   }
+// }
 
-function stopLiveLocation(id) {
-  window.navigator.geolocation.clearWatch(id);
-  liveLocationBtn.style.display = "inline";
-  cancelLiveLocationBtn.style.display = "none";
-}
+// function stopLiveLocation(id) {
+//   window.navigator.geolocation.clearWatch(id);
+//   liveLocationBtn.style.display = "inline";
+//   cancelLiveLocationBtn.style.display = "none";
+// }
 
-function liveLocation() {
-  const id = windows.navigator.geolocation.watchPosition(
-    positionCallback,
-    errorCallback,
-    { enableHighAccuracy: true, }
-  );
+// function liveLocation() {
+//   const id = windows.navigator.geolocation.watchPosition(
+//     positionCallback,
+//     errorCallback,
+//     { enableHighAccuracy: true, }
+//   );
 
-  liveLocationBtn.style.display = "none";
-  cancelLiveLocationBtn.style.display = "inline";
-  cancelLiveLocationBtn.onclick = function (e) {
-    stopLiveLocation(id);
-  };
-}
+//   liveLocationBtn.style.display = "none";
+//   cancelLiveLocationBtn.style.display = "inline";
+//   cancelLiveLocationBtn.onclick = function (e) {
+//     stopLiveLocation(id);
+//   };
+// }
 
-function getLocation() {
-  window.navigator.geolocation.getCurrentPosition(
-    positionCallback,
-    errorCallback,
-    { enableHighAccuracy: true, }
-  );
-}
+// function getLocation() {
+//   window.navigator.geolocation.getCurrentPosition(
+//     positionCallback,
+//     errorCallback,
+//     { enableHighAccuracy: true, }
+//   );
+// }
 
-function getValue(value, unit = "", fix = 0) {
-  return value === null ? "NA" : value.toFixed(fix) +
-    " " + unit;
-}
+// function getValue(value, unit = "", fix = 0) {
+//   return value === null ? "NA" : value.toFixed(fix) +
+//     " " + unit;
+// }
 
-function getSpeedInMi(value, unit = "", fix = 0) {
-  return value === null ? "NA" : (value * 1609.344).toFixed(fix) +
-    " " + unit;
-}
+// function getSpeedInMi(value, unit = "", fix = 0) {
+//   return value === null ? "NA" : (value * 1609.344).toFixed(fix) +
+//     " " + unit;
+// }
 
-function positionCallback(position) {
-  longitude.innerText = getValue(position.coords.longitude,
-    "°", 6);
-  latitude.innerText = getValue(position.coords.latitude,
-    "°", 6);
-  accuracy.innerText = getValue(position.coords.accuracy,
-    "m", 2);
-  altitude.innerText = getValue(position.coords.altitude,
-    "m", 2);
-  altitudeAccuracy.innerText = getValue(
-    position.coords.altitudeAccuracy,
-    "m",
-    2
-  );
-  heading.innerText = getValue(position.coords.heading,
-    "°", 2);
-  speed.innerText = getSpeedInMi(position.coords.speed, "mi/s", 2);
-  time.innerText =
-    position.timestamp === null
-      ? "NA"
-      : new Date(position.timestamp).toLocaleTimeString();
-}
+// function positionCallback(position) {
+//   longitude.innerText = getValue(position.coords.longitude,
+//     "°", 6);
+//   latitude.innerText = getValue(position.coords.latitude,
+//     "°", 6);
+//   accuracy.innerText = getValue(position.coords.accuracy,
+//     "m", 2);
+//   altitude.innerText = getValue(position.coords.altitude,
+//     "m", 2);
+//   altitudeAccuracy.innerText = getValue(
+//     position.coords.altitudeAccuracy,
+//     "m",
+//     2
+//   );
+//   heading.innerText = getValue(position.coords.heading,
+//     "°", 2);
+//   speed.innerText = getSpeedInMi(position.coords.speed, "mi/s", 2);
+//   time.innerText =
+//     position.timestamp === null
+//       ? "NA"
+//       : new Date(position.timestamp).toLocaleTimeString();
+// }
 
-function errorCallback(err) {
-  let msg;
-  switch (err.code) {
-    case err.PERMISSION_DENIED:
-      msg = "DENIED ACCESS FOR LOCATION";
-      break;
-    case err.POSITION_UNAVAILABLE:
-      msg = "LOCATION INFO UNAVAILABLE";
-      break;
-    case err.TIMEOUT:
-      msg = "REQUEST TIMED OUT";
-      break;
-    case err.UNKNOWN_ERROR:
-      msg = "UNKNOWN ERROR OCCURRED";
-      break;
-  }
-  mainSection.style.display = "none";
-  actions.style.display = "none";
-  error.style.display = "block";
-  errorMessage.innerText = msg;
-  cancelLiveLocationBtn.click();
-}
+// function errorCallback(err) {
+//   let msg;
+//   switch (err.code) {
+//     case err.PERMISSION_DENIED:
+//       msg = "DENIED ACCESS FOR LOCATION";
+//       break;
+//     case err.POSITION_UNAVAILABLE:
+//       msg = "LOCATION INFO UNAVAILABLE";
+//       break;
+//     case err.TIMEOUT:
+//       msg = "REQUEST TIMED OUT";
+//       break;
+//     case err.UNKNOWN_ERROR:
+//       msg = "UNKNOWN ERROR OCCURRED";
+//       break;
+//   }
+//   mainSection.style.display = "none";
+//   actions.style.display = "none";
+//   error.style.display = "block";
+//   errorMessage.innerText = msg;
+//   cancelLiveLocationBtn.click();
+// }
 
-main();
+// main();
